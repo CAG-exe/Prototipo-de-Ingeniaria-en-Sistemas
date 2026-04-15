db8de691834bd721c1bed25f4ff1e36b9945b4c2
Claudio <andrewgalar@gmail.com>
2026-04-15 14:51:47 -0300

    pagina de insripcion a talleres

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { MainScreen } from './pages/main-screen/main-screen';
 import { ModeratorRegistration } from './pages/moderator-registration/moderator-registration';
 import { CollaboratorRegistration } from './pages/collaborator-registration/collaborator-registration';
+import { FormularioInscripcionTalleres } from './pages/formulario-inscripcion-talleres/formulario-inscripcion-talleres';
 
 export const routes: Routes = [
   { path: '', component: MainScreen },
   { path: 'registro-moderador', component: ModeratorRegistration },
   { path: 'registro-colaborador', component: CollaboratorRegistration },
+  { path: 'formulario-inscripcion-talleres', component: FormularioInscripcionTalleres },
   { path: '**', redirectTo: '' }
 ];
