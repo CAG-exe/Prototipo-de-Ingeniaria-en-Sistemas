0e257dde17fe4c0f5f3b2013eeed89a51e519023
Alejandro <correa92.dev@gmail.com>
2026-04-18 10:03:57 -0300

    Se elimina componente y se redirecciona a form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { MainScreen } from './pages/main-screen/main-screen';
 import { ModeratorRegistration } from './pages/moderator-registration/moderator-registration';
-import { CollaboratorRegistration } from './pages/collaborator-registration/collaborator-registration';
 import { FormularioInscripcionTalleres } from './pages/formulario-inscripcion-talleres/formulario-inscripcion-talleres';
 import { WorkshopDetailComponent } from './pages/WorkshopDetail/WorkshopDetail.component';
 import { WorkshopDetailsListComponent } from './pages/ListWorkshopDetail/WorkshopDetailsList.component';
@@ -9,8 +8,7 @@ import { WorkshopDetailsListComponent } from './pages/ListWorkshopDetail/Worksho
 export const routes: Routes = [
   { path: '', component: MainScreen },
   { path: 'registro-moderador', component: ModeratorRegistration },
-  { path: 'registro-colaborador', component: CollaboratorRegistration },
-  { path: 'formulario-inscripcion-talleres', component: FormularioInscripcionTalleres },
+  { path: 'registro-colaborador', component: FormularioInscripcionTalleres },
   { path: 'taller/:id', component: WorkshopDetailComponent },
   { path: 'talleres', component: WorkshopDetailsListComponent },
   { path: '**', redirectTo: '' }
